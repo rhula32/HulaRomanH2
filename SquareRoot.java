9ce95ab27e99bb3796150adc0a9a724091c6aad9
rhula32 <rhula32@gmail.com>
2016-09-29 23:44:45 -0400

    Final Project Ready For Submission
    After a long hard struggle, the code is compiled and is working great!

--- SquareRoot.java
+++ SquareRoot.java
@@ -3,59 +3,46 @@ import java.util.Scanner; //Needed for user input
 //Declare class name
 public class SquareRoot
 {
-  private static double number;
-  private static double guess;
-  private static double error;
-  
   //Declare the main class
   public static void main(String[] args)
   {
-    //Constructor of new object in class
-    SquareRoot bullshit = new SquareRoot();
+    //Constructor
+    SquareRoot newNumber = new SquareRoot();
     
-    //Needed for user input
+    //Create a scanner object to accept input from the user
     Scanner in = new Scanner(System.in);
     
     //Ask the user for the number they'd like to find the square root of
     System.out.println("What number would you like to find the square root of?");
-    double number = in.nextDouble();
+    double desiredNumber = in.nextDouble();
     
-    //Ask the user for the amount of error they'd prefer
+    //Ask the user for the amount of error the want
     System.out.println("How much error will you accept?");
-    double error = in.nextDouble();
+    double desiredError = in.nextDouble();
     
-    //CALLING ALL METHODS!
-    bullshit.getNumber();
-    bullshit.getError();
-    System.out.println(bullshit.findSquareRoot());
+    //Declare & initiate the inital guess variable for the first condition of the algorithm
+    double initialGuess = desiredNumber/2;
     
+    //Print out the output, which should be the square root of the number entered.
+    System.out.println("The square root is: " + findSquareRoot(desiredNumber, initialGuess, desiredError));
   }
   
-  //Create the findSquareRoot method; This will obtain the square root of the number the user entered, adjusted for error.
-  public static double findSquareRoot()
-  {
-    guess = (0.5)*number;
-    
-    if((number - (guess*guess)) < error)
+  //Create the findSquareRoot method; This will find the square root of the number the user entered
+  //It will also account for the error specified by the user
+  //"Math.abs" is used to avoid having a negative number run through the if statement test
+  //If a negative number were to run through, the recursive method would stop calling itself
+  public static double findSquareRoot(double number, double guess, double error)
+  { 
+    if(Math.abs(number - guess*guess) <= error)
     {
       return guess;
     }
     else
     {
       guess = (0.5)*(guess + number/guess);
-      return findSquareRoot();
+      return findSquareRoot(number, guess, error);
     }
   }
-  
-  public static double getNumber()
-  {
-    return number;
-  }
-  
-  public static double getError()
-  {
-    return error;
-  }
 }
-    
+
   
